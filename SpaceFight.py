982ddd22066dfaa1b073c6bfad8efb701b199a8e
udit034 <udit.mehata@gmail.com>
2018-05-26 15:53:39 -0700

    Added functionality to change speed and direction

--- SpaceFight.py
+++ SpaceFight.py
@@ -12,15 +12,44 @@ turtle.tracer(1)
 '''
 
 class Sprite( turtle.Turtle ):
-	def __init__( self, spriteshape, color, startx, starty ):
+	def __init__( self, spriteshape, color, startx, starty, speed ):
 		turtle.Turtle.__init__( self, shape=spriteshape )
-		self.speed( 0 )
 		self.penup()
 		self.color( color )
 		self.fd( 0 )
 		self.goto( startx, starty ) 
-		self.speed = 1
+		self.speed = speed
 		
-player = Sprite( "triangle", "white", 0, 0 )
+	def move( self ):
+		self.fd( self.speed )
+		
+class Player( Sprite ):
+	def __init__( self, spriteshape, color, startx, starty, speed, lives ):
+		Sprite.__init__( self, spriteshape, color, startx, starty, speed )
+		self.lives = lives
+		
+	def accelerate( self ):
+		self.speed += 1
+		
+	def decelerate( self ):
+		self.speed -= 1
+		
+	def turn_left( self ):
+		self.lt( 45 )
+		
+	def turn_right( self ):
+		self.rt( 45 )
+		
+player = Player( "triangle", "white", 0, 0, 1, 3 )
+turtle.onkey( player.turn_left, "Left" )
+turtle.onkey( player.turn_right, "Right" )
+turtle.onkey( player.accelerate, "Up" )
+turtle.onkey( player.decelerate, "Down" )
+turtle.listen()
+
+while True:
+	player.move()
+	
+
 
 delay = input( "Enter" )
