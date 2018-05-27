50781367dd67bfa444825b2a222c10b0657747b8
udit034 <udit.mehata@gmail.com>
2018-05-26 17:09:46 -0700

    Set up the Game Class and boundary box logic

--- SpaceFight.py
+++ SpaceFight.py
@@ -23,8 +23,11 @@ class Sprite( turtle.Turtle ):
 	def move( self ):
 		self.fd( self.speed )
 		
+		if self.xcor() > 290 or self.ycor() > 290 or self.xcor() < -290 or self.ycor() < -290:
+			self.rt(60)
+		
 class Player( Sprite ):
-	def __init__( self, spriteshape, color, startx, starty, speed, lives ):
+	def __init__( self, spriteshape, color, startx=0, starty=0, speed=1, lives=3 ):
 		Sprite.__init__( self, spriteshape, color, startx, starty, speed )
 		self.lives = lives
 		
@@ -39,6 +42,31 @@ class Player( Sprite ):
 		
 	def turn_right( self ):
 		self.rt( 45 )
+		
+class Game():
+	def __init__(self, level=1, score=0, state="playing", lives=3 ):
+		self.level = level
+		self.score = score
+		self.state = state
+		self.lives = lives
+		self.pen = turtle.Turtle()
+	
+	def draw_border( self ):
+		self.pen.speed( 0 ) 
+		self.pen.color( "white" )
+		self.pen.pensize( 3 )
+		self.pen.penup()
+		self.pen.goto( -300, 300 )
+		self.pen.pendown()
+		for side in range(4):
+			self.pen.fd( 600 )
+			self.pen.rt( 90 )
+		self.pen.penup()
+		self.pen.ht()
+		
+		
+game = Game()		
+game.draw_border()
 		
 player = Player( "triangle", "white", 0, 0, 1, 3 )
 turtle.onkey( player.turn_left, "Left" )
